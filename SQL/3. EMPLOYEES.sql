ebc99c371fd87c569206944bc2a9a0effab835c2
moazmohamed20 <moazmohamed15@outlook.com>
2022-06-07 00:48:23 +0200

    Insert Dummy Data

---
+++ SQL/3. EMPLOYEES.sql
@@ -0,0 +1,6 @@
+INSERT INTO EMPLOYEES VALUES ('admin', 'admin');
+INSERT INTO EMPLOYEES VALUES ('moazohamed20', 'moazmohmed');
+INSERT INTO EMPLOYEES VALUES ('mustafaamin28', 'mustafa');
+INSERT INTO EMPLOYEES VALUES ('muhammad1596', 'muhammad');
+INSERT INTO EMPLOYEES VALUES ('mostafalotfe', 'mostafa');
+INSERT INTO EMPLOYEES VALUES ('mooostafa8900', 'mooostafa');
